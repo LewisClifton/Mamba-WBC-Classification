a2c7227aedf4601939396ea1009aacbec8361e86
LewisClifton <lewisclifton2003@gmail.com>
2025-01-30 15:45:55 +0000

    removed wrong dtype

--- data/dataset.py
+++ data/dataset.py
@@ -76,7 +76,7 @@ class TransformedDataset(Dataset):
         image, label = self.dataset[idx]
 
         image = self.transform(image)
-        label = torch.tensor(label, dtype=torch.long)
+        label = torch.tensor(label, dtype=torch.uint8)
         
         return image, label
 
