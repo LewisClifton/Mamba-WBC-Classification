ae923fd4fc4493e4a44e2ff05cdf0ee6023f4f70
LewisClifton <lewisclifton2003@gmail.com>
2025-01-30 16:45:45 +0000

    added roc auc

--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ import torch.distributed as dist
 import torch.nn as nn
 from torchvision.models import swin_t, swin_s, swin_b
 from torchvision import transforms
+import torch.nn.functional as F
+from sklearn.metrics import roc_auc_score
 
 
 def write_dict_to_file(file, dict_):
@@ -154,14 +156,17 @@ def train_loop(model, train_loader, val_loader, n_epochs, criterion, optimizer, 
     """
     train_accuracy_per_epoch = []
     train_loss_per_epoch = []
+    train_roc_auc_per_epoch = []
     val_accuracy_per_epoch = []
     val_loss_per_epoch = []
+    val_roc_auc_per_epoch = []
 
     for epoch in range(n_epochs):
         model.train()
         train_loss = 0.0
         correct_train = 0
         total_train = 0
+        train_roc_auc = 0
 
         for images, labels in train_loader:
             images, labels = images.to(device), labels.to(device)
@@ -182,16 +187,20 @@ def train_loop(model, train_loader, val_loader, n_epochs, criterion, optimizer, 
             _, preds = outputs.max(1)
             correct_train += (preds == labels).sum().item()
             total_train += labels.size(0)
+            probabilities = F.softmax(outputs, dim=1)[:, 1]
+            train_roc_auc += (roc_auc_score(labels, probabilities)) 
 
         # Calculate training metrics
         avg_train_loss = train_loss / len(train_loader)
         train_accuracy = correct_train / total_train
+        avg_train_roc_auc = train_roc_auc / total_train 
 
         # Validation loop
         model.eval()
         val_loss = 0.0
         correct_val = 0
         total_val = 0
+        val_roc_auc = 0
 
         with torch.no_grad():
             for images, labels in val_loader:
@@ -200,20 +209,27 @@ def train_loop(model, train_loader, val_loader, n_epochs, criterion, optimizer, 
                 outputs = model(images)
                 loss = criterion(outputs, labels.squeeze(dim=1))
 
+                probabilities = F.softmax(outputs, dim=1)[:, 1]
+
                 val_loss += loss.item()
                 _, preds = outputs.max(1)
                 correct_val += (preds == labels).sum().item()
                 total_val += labels.size(0)
+                probabilities = F.softmax(outputs, dim=1)[:, 1]
+                val_roc_auc += (roc_auc_score(labels, probabilities)) 
 
         # Calculate validation metrics
         avg_val_loss = val_loss / len(val_loader)
         val_accuracy = correct_val / total_val
+        avg_val_roc_auc = val_roc_auc / total_val
 
         # Store metrics for this epoch
         train_accuracy_per_epoch.append(train_accuracy)
         train_loss_per_epoch.append(avg_train_loss)
+        train_roc_auc_per_epoch.append(avg_train_roc_auc)
         val_accuracy_per_epoch.append(val_accuracy)
         val_loss_per_epoch.append(avg_val_loss)
+        val_roc_auc_per_epoch.append(avg_val_roc_auc)
 
         # Print epoch metrics
         if device == 0:
@@ -226,15 +242,19 @@ def train_loop(model, train_loader, val_loader, n_epochs, criterion, optimizer, 
     if using_dist:
         # Average per epoch metrics across over all GPUs
         return model, {
-            'Average Train Accuracy per epoch': average_across_gpus(train_accuracy_per_epoch, device),
-            'Average Train Loss per epoch': average_across_gpus(train_loss_per_epoch, device),
-            'Average Validation Accuracy per epoch': average_across_gpus(val_accuracy_per_epoch, device),
-            'Average Validation Loss per epoch': average_across_gpus(val_loss_per_epoch, device)
+            'Average train Accuracy per epoch': average_across_gpus(train_accuracy_per_epoch, device),
+            'Average train Loss per epoch': average_across_gpus(train_loss_per_epoch, device),
+            'Average train ROC AUC per epoch': average_across_gpus(train_roc_auc_per_epoch),
+            'Average validation Accuracy per epoch': average_across_gpus(val_accuracy_per_epoch, device),
+            'Average validation Loss per epoch': average_across_gpus(val_loss_per_epoch, device),
+            'Average validation ROC AUC per epoch': average_across_gpus(val_roc_auc_per_epoch),
         }
     
     return model, {
         'Train Accuracy per epoch': train_accuracy_per_epoch,
         'Train Loss per epoch': train_loss_per_epoch,
+        'Train ROC AUC per epoch': train_roc_auc_per_epoch,
         'Validation Accuracy per epoch': val_accuracy_per_epoch,
-        'Validation Loss per epoch': val_loss_per_epoch
+        'Validation Loss per epoch': val_loss_per_epoch,
+        'Validation ROC AUC per epoch': val_roc_auc_per_epoch,
     }
