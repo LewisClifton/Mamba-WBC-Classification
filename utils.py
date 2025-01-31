c8c867e1e493f22e8599f8b700ca64db16b6b49d
LewisClifton <lewisclifton2003@gmail.com>
2025-01-31 10:25:05 +0000

    Hopefully fixed dims

--- utils.py
+++ utils.py
@@ -270,7 +270,7 @@ def train_loop(model, train_loader, val_loader, n_epochs, criterion, optimizer, 
             _, preds = outputs.max(1)
             correct_train += (preds == labels).sum().item()
             total_train += labels.size(0)
-            probabilities = F.softmax(outputs, dim=1)[:, 1]
+            probabilities = F.softmax(outputs, dim=1)[:, 1].unsqueeze(1)
             print(outputs.shape)
             print(probabilities.shape)
             train_roc_auc += (roc_auc_score(labels.detach().cpu(), probabilities.detach().cpu(), multi_class='ovr')) 
