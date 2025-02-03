54572993e414e6271e9b56c8b131b3d64d73b981
LewisClifton <lewisclifton2003@gmail.com>
2025-02-03 15:08:48 +0000

    removed another medmamba ref

--- train.py
+++ train.py
@@ -175,9 +175,9 @@ if __name__ == '__main__':
     with open(config_path, 'r') as yml:
         config = yaml.safe_load(yml)
 
-    # Multi GPU not supported for windows and trivially not for 1 GPU and not for MedMamba
+    # Multi GPU not supported for windows and trivially not for 1 GPU
     using_dist = True
-    if using_windows or num_gpus == 1 or config['model']['type'] == 'medmamba':
+    if using_windows or num_gpus == 1:
         using_dist = False
 
     # Create process group if using multi gpus on Linux
