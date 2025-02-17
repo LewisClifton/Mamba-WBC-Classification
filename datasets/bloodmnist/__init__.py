bb423e552d80453c4ab0fe246a12fb6f69a2a72e
LewisClifton <lewisclifton2003@gmail.com>
2025-02-17 18:00:02 +0000

    changes BloodMnist download location

--- datasets/bloodmnist/__init__.py
+++ datasets/bloodmnist/__init__.py
@@ -1,13 +1,15 @@
+import os
+
 from medmnist import BloodMNIST
 
 def get(test=False):
 
     if not test:
-        train_dataset = BloodMNIST(split='train', download=True, size=224)
-        val_dataset = BloodMNIST(split='val', download=True, size=224)
+        train_dataset = BloodMNIST(split='train', download=True, size=224, root=os.path.join(os.getcwd(), 'datasets/bloodmnist/data/'))
+        val_dataset = BloodMNIST(split='val', download=True, size=224, root=os.path.join(os.getcwd(), 'datasets/bloodmnist/data/'))
         return train_dataset, val_dataset
     else: 
-        test_dataset = BloodMNIST(split='test', download=True, size=224)
+        test_dataset = BloodMNIST(split='test', download=True, size=224, root=os.path.join(os.getcwd(), 'datasets/bloodmnist/data/'))
         return test_dataset
 
     
