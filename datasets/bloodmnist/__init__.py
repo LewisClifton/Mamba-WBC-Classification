589eac2bd3382e56b33cde0b0be5f3027e10bc7e
LewisClifton <lewisclifton2003@gmail.com>
2025-02-17 18:38:17 +0000

    hopefully fixed bloodmnist

--- datasets/bloodmnist/__init__.py
+++ datasets/bloodmnist/__init__.py
@@ -8,14 +8,23 @@ def get(dataset_download_dir, test=False):
     if dataset_download_dir:
         if not os.path.isdir(dataset_download_dir):
             os.mkdir(dataset_download_dir)
-    else:
-        dataset_download_dir = "~/.medmnist"
 
-    # Get the dataset
-    if not test:
-        train_dataset = BloodMNIST(split='train', download=True, size=224, root=dataset_download_dir)
-        val_dataset = BloodMNIST(split='val', download=True, size=224, root=dataset_download_dir)
-        return train_dataset, val_dataset
-    else: 
-        test_dataset = BloodMNIST(split='test', download=True, size=224, root=dataset_download_dir)
-        return test_dataset
+        # Get the dataset
+        if not test:
+            train_dataset = BloodMNIST(split='train', download=True, size=224, root=dataset_download_dir)
+            val_dataset = BloodMNIST(split='val', download=True, size=224, root=dataset_download_dir)
+            return train_dataset, val_dataset
+        else: 
+            test_dataset = BloodMNIST(split='test', download=True, size=224, root=dataset_download_dir)
+            return test_dataset
+        
+    else:
+
+        # Get the dataset
+        if not test:
+            train_dataset = BloodMNIST(split='train', download=True, size=224)
+            val_dataset = BloodMNIST(split='val', download=True, size=224)
+            return train_dataset, val_dataset
+        else: 
+            test_dataset = BloodMNIST(split='test', download=True, size=224)
+            return test_dataset
