cb134aef5ba9bfe655ee6674d2b6089b113e817b
LewisClifton <lewisclifton2003@gmail.com>
2025-02-24 22:18:07 +0000

    fixed vmamba initialisation

--- models/vmamba/__init__.py
+++ models/vmamba/__init__.py
@@ -28,16 +28,16 @@ def build_model(model_size='tiny'):
 
     if model_size == 'tiny':
         return vmamba(
-            depths=[2, 2, 8, 2], dims=96, drop_path_rate=0.2, 
-            patch_size=4, in_chans=3, num_classes=1000, 
-            ssm_d_state=1, ssm_ratio=1.0, ssm_dt_rank="auto", ssm_act_layer="silu",
-            ssm_conv=3, ssm_conv_bias=False, ssm_drop_rate=0.0, 
-            ssm_init="v0", forward_type="v05_noz", 
-            mlp_ratio=4.0, mlp_act_layer="gelu", mlp_drop_rate=0.0, gmlp=False,
-            patch_norm=True, norm_layer="ln2d", 
-            downsample_version="v3", patchembed_version="v2", 
-            use_checkpoint=False, posembed=False, imgsize=224, 
-        )
+                depths=[2, 2, 8, 2], dims=96, drop_path_rate=0.2,
+                patch_size=4, in_chans=3, num_classes=1000,
+                ssm_d_state=1, ssm_ratio=1.0, ssm_dt_rank="auto", ssm_act_layer="silu",
+                ssm_conv=3, ssm_conv_bias=False, ssm_drop_rate=0.0,
+                ssm_init="v0", forward_type="v05_noz",
+                mlp_ratio=4.0, mlp_act_layer="gelu", mlp_drop_rate=0.0, gmlp=False,
+                patch_norm=True, norm_layer="ln2d",
+                downsample_version="v3", patchembed_version="v2",
+                use_checkpoint=False, posembed=False, imgsize=224,
+            )
     elif model_size == 'small':
         return vmamba(
             depths=[2, 2, 15, 2], dims=96, drop_path_rate=0.3, 
