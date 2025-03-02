5ecf070f9d94546cc456379fcd7c082d634e0831
LewisClifton <lewisclifton2003@gmail.com>
2025-03-02 13:23:48 +0000

    added neutrophil secondary classifier

--- eval.py
+++ eval.py
@@ -6,6 +6,7 @@ import time
 
 import torch
 from torch.utils.data import DataLoader
+import torch.nn as nn
 
 from datasets import get_dataset, TransformedDataset
 from models import init_model
@@ -41,13 +42,55 @@ def evaluate_model(model, test_loader, device):
             images, labels = images.to(device), labels.to(device)
 
             outputs = model(images)
-            _, preds = outputs.max(1)
+            if not isinstance(model, CompleteClassifier):
+                outputs = torch.argmax(outputs, dim=1)
 
-            all_preds.extend(preds.cpu().numpy().tolist())
+            all_preds.extend(outputs.cpu().numpy().tolist())
             all_labels.extend(labels.cpu().numpy().tolist())
 
     # Return evaluation metrics
     return get_eval_metrics(all_preds, all_labels)
+
+
+# Two-tier model for neutrophils classification
+class CompleteClassifier(nn.Module):
+    def __init__(self, model_config, dataset_config):
+
+        # Load the wbc classifier
+        self.wbc_model, self.model_transforms = init_model(model_config, dataset_config['n_classes'])
+        self.wbc_model.load_state_dict(torch.load(model_config['trained_model_path'], map_location='cpu'))
+        self.wbc_model.eval()
+
+        # Get the indices of the SNE and BNE neutrophils classes
+        self.BNE_index = dataset_config['classes'].index('BNE')
+        self.SNE_index = dataset_config['classes'].index('SNE')
+        self.neutrophils_indices = torch.tensor([self.BNE_index, self.SNE_index])
+
+        # Load the neutrophils classifier
+        model_config['trained_model_path'] = model_config['neutrophil_model_path']
+        self.neutrophils_model, _ = init_model(model_config, num_classes=2)
+        self.neutrophils_model.load_state_dict(torch.load(model_config['neutrophil_model_path'], map_location='cpu'))
+        self.neutrophils_model.eval()
+
+    def forward(self, x):
+        wbc_out = self.wbc_model(x)
+        wbc_type = torch.argmax(wbc_out, dim=1)
+
+         # Mask for images classified as neutrophils
+        neutrophil_mask = torch.isin(wbc_type, self.neutrophils_indices)
+
+        if neutrophil_mask.any():
+            # Get indices of images classified as neutrophils
+            neutrophil_indices = torch.where(neutrophil_mask)[0]
+
+            # Get neutrophil type prediction
+            neutrophil_out = self.neutrophils_model(x[neutrophil_indices])
+            neutrophil_type = torch.argmax(neutrophil_out, dim=1)
+
+            # Map the neutrophils binary predictions to WBC classes
+            wbc_type[neutrophil_indices] = torch.where(neutrophil_type == 0, self.BNE_index, self.SNE_index)
+        
+        return wbc_type
 
 
 def main(out_dir, model_config, dataset_config, dataset_download_dir):
@@ -56,11 +99,13 @@ def main(out_dir, model_config, dataset_config, dataset_download_dir):
     device = 'cuda'
 
     # Load model
-    model, model_transforms = init_model(model_config, dataset_config['n_classes'])
-
-    # Load saved weights
-    model.load_state_dict(torch.load(model_config['trained_model_path'], map_location=device))
-    model.eval()
+    if 'neutrophil_model_path' in model_config: 
+        model = CompleteClassifier(model_config, dataset_config)
+        model_transforms = model.model_transforms
+    else: 
+        model, model_transforms = init_model(model_config, dataset_config['n_classes'])
+        model.load_state_dict(torch.load(model_config['trained_model_path'], map_location=device))
+        model.eval()
 
     # Put on device
     model.to(device)
@@ -97,6 +142,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--out_dir', type=str, help='Path to directory where model evaluation log will be saved (default=cwd)', default='.')
     parser.add_argument('--trained_model_path', type=str, help='Path to trained model .pth', required=True)
+    parser.add_argument('--neutrophil_model_path', type=str, help='Path to trained neutrophil model .pth')
     parser.add_argument('--model_type', type=str, help='Model type e.g. "swin", "vmamba" ', required=True)
     parser.add_argument('--batch_size', type=int, help='Batch size when evaluating', default=32)
     parser.add_argument('--dataset_config_path', type=str, help='Path to dataset .yml used for evaluation', required=True)
@@ -114,6 +160,8 @@ if __name__ == "__main__":
         'name' : args.model_type,
         'batch_size' : args.batch_size,
     }
+    if args.neutrophil_model_path: model_config['neutrophil_model_path'] = args.neutrophil_model_path
+
     with open(dataset_config_path, 'r') as yml:
         dataset_config = yaml.safe_load(yml)
 
