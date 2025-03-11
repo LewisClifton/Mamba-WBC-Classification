b7eed268909585a858f07e6bfe875aae72dd5323
LewisClifton <lewisclifton2003@gmail.com>
2025-03-11 21:53:37 +0000

    fixed ensemble loading

--- utils_ensemble/common.py
+++ utils_ensemble/common.py
@@ -3,16 +3,18 @@ import torch
 from models import init_model
 from models.ensemble import Ensemble
 
+
 def get_ensemble(ensemble_config, num_classes, device):
 
     base_models = []
     base_models_transforms = []
 
-    
     base_models_classes = None if 'stacking_model_path' in ensemble_config else num_classes
 
     for base_model_config in ensemble_config['models']:
-        base_model, base_model_transform = init_model(base_model_config, num_classes=base_models_classes, device=device)
+        base_model, base_model_transform = init_model(base_model_config, base_models_classes, device)
+
+        base_model.load_state_dict(torch.load(base_model_config['trained_model_path'], map_location=device))
         base_model.eval()
 
         base_models.append(base_model)
